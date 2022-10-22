eda73fc7634914269aefb7cba9349cb5d359cf89
Konstantin Serov <konstantin.p.serov@gmail.com>
2022-10-22 21:24:34 +0700

    pubspec edits

--- lib/lite_state.dart
+++ lib/lite_state.dart
@@ -2,7 +2,6 @@ import 'dart:async';
 import 'dart:convert';
 import 'dart:io' as io;
 
-// ignore: depend_on_referenced_packages
 import 'package:collection/collection.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
